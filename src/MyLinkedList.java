3c2227e99e3e44e6456d0e85fbc99456ee6ae49d
lab1 <zhanerketkva7@gmail.com>
2023-04-19 20:39:52 +0600

    MyLinkedList

---
+++ src/MyLinkedList.java
@@ -0,0 +1,71 @@
+public class MyLinkedList<E> implements MyList{
+    private class Node<E> {
+        E val;
+        Node previous;
+        Node next;
+        public Node(E v) {
+            val = v;
+
+        }
+    }
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
