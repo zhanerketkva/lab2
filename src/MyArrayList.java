bf0ada1a07d2e5ae57205eccbd058f01a3c758cc
lab1 <zhanerketkva7@gmail.com>
2023-04-19 18:38:39 +0600

    MyArrayList.java

---
+++ src/MyArrayList.java
@@ -0,0 +1,43 @@
+public class MyArrayList<T> implements MyList {
+    private T[] arr;
+    private int size;
+
+    MyArrayList(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    public void increaseBuffer(){
+        T[] newArr = (T[]) new Object[arr.length*2];
+        for(int i=0; i< arr.length; i++){
+            newArr[i]=arr[i];
+        }
+        arr = newArr;
+    }
+
+    public T getElement(int index) {
+        checkIndex(index);
+        return arr[index];
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public void clear(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    public void delete(int index){
+        checkIndex(index);
+        for(int i= index + 1; i<size; i++){
+            arr[i-1] = arr[i];
+        }
+        size--;
+    }
+    public void checkIndex(int index){
+        if(index < 0 || index>=size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+}
