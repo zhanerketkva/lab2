d5ce1eca5ddf69e8da91dda465c9b588c4d09eda
lab1 <zhanerketkva7@gmail.com>
2023-04-19 19:09:10 +0600

    methods

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -23,10 +23,57 @@ public class MyArrayList<T> implements MyList {
     public int getSize() {
         return size;
     }
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+        if(size == arr.length){
+            increaseBuffer();
+        }
+        arr[size++] = (T) item;
+    }
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
     public void clear(){
         this.arr = (T[]) new Object[5];
         this.size = 0;
     }
+
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+
 
     public void delete(int index){
         checkIndex(index);
